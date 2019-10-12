c631cdc524227c10a467047691ddd453ed4a38f5
Michał Basiński <michal.basinski88@gmail.com>
2019-10-12 12:41:17 +0200

    temp

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("aaaaa");
+        System.out.println("bbbbb");
         System.out.println( "Hello World!" );
     }
 }
--- src/main/java/pl/sda/checksums/LuhnVerifier.java
+++ src/main/java/pl/sda/checksums/LuhnVerifier.java
@@ -1,9 +1,19 @@
 package pl.sda.checksums;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LuhnVerifier implements ChecksumVerifier {
     @Override
     public boolean verify(String number) {
         //TODO: zaimplementować algorytm Luhna
-        return false;
+        int i = 0;
+        List<Integer> lista = new ArrayList<>();
+        lista.add(1);
+        lista.add(1222);
+        lista.add(1456);
+        lista.add(187);
+        lista.add(423421);
+        return lista.size() == 50;
     }
 }
